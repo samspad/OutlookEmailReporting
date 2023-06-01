0aed3345a5ea7d9740f6415ba666b0314343dc34
Ali <shahidali@marshall.edu>
2023-06-01 15:08:56 -0400

    modification in requirement

--- index.py
+++ index.py
@@ -4,6 +4,14 @@ import win32com.client as win32
 import getpass
 import csv
 
+def get_time_by_subject(subject):
+    if subject == 'Start and End Time for LCAP-ExportLoanPdsCompleted Normally':
+        return '4:00 PM'
+    elif subject == 'Start and End Time for BatchLoadSAORDTS_CFTFact_dailyCompleted Normally':
+        return '7:00 PM'
+    else:
+        return ''
+
 def read_outlook_emails(subject, csv_file):
     outlook_app = win32.Dispatch("Outlook.Application")
     namespace = outlook_app.GetNamespace("MAPI")
@@ -12,6 +20,7 @@ def read_outlook_emails(subject, csv_file):
     items = inbox.Items
     items.Sort("[ReceivedTime]", True)  # Sort emails by received time in descending order
 
+    rows = []
     for item in items:
         if item.Subject == subject:
             #print("Subject:", item.Subject)
@@ -19,30 +28,41 @@ def read_outlook_emails(subject, csv_file):
             #print("Body:", item.Body)
             
             body_lines = item.Body.split("\n")
-            rows = []
-            for i in range(0, len(body_lines), 3):
-                milestone = body_lines[i].strip()
-                time = body_lines[i+1].strip()
-                completed_time = body_lines[i+2].strip()
-                rows.append([time, milestone, completed_time])
+            
+            #for i in range(0, len(body_lines), 3):
+            milestone = body_lines[0].strip()
+            start_time = body_lines[1].strip()
+            end_time = body_lines[2].strip()
+            time = get_time_by_subject(item.Subject)
+            
+            status = ""
+            if end_time < time:
+                status = "✔️ (Green)"
+            else:
+                status = "✔️ (Yellow)"
+            
+            rows.append([time, milestone, status, f"Start Time: {start_time}\nEnd Time: {end_time}"])
 
             with open(csv_file, mode='w', newline='') as file:
                 writer = csv.writer(file)
-                writer.writerow(["Time", "Milestone", "Completed Time"])
+                writer.writerow(["Time", "Milestone", "Status", "Completed Time (Populate miss occurred)"])
                 writer.writerows(rows)
             print(f"CSV file '{csv_file}' created successfully!")
             break  # Assuming you only want to process the first matching email. If we remove this, it will look for all the emails with matching subject line
 
-    outlook_app.Quit()
+    #outlook_app.Quit()
 
-# Example usage
-subject_to_search = "Start and End Time for LCAP-ExportLoanPdsCompleted Normally"
+# Subject search examples
+subject_to_search = [
+    'Start and End Time for LCAP-ExportLoanPdsCompleted Normally',
+    'Start and End Time for BatchLoadSAORDTS_CFTFact_dailyCompleted Normally'
+]
 output_csv_file = "email_data.csv"
 
 
 # Prompt for Outlook credentials. This will be moved to .env file so that the credentials are hidden. We can
-username = input("Enter your Outlook username: ")
-password = getpass.getpass("Enter your Outlook password: ")
+username = input("Enter your outlook username: ")
+password = getpass.getpass("Enter your outlook password: ")
 
 # Authenticate and read emails
 outlook_app = win32.Dispatch("Outlook.Application")
@@ -51,5 +71,5 @@ namespace.Logon(username, password, True, False)
 
 read_outlook_emails(subject_to_search, output_csv_file)
 
-# Log off (this will close the outlook application)
-#namespace.Logoff()
+# Log off
+namespace.Logoff()
